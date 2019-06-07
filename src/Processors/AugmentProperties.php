c0ba767f5272b04fc6307ed92ad939d5298a8991
Tobias Stursberg <tobias.stursberg@jameda.de>
2019-06-07 18:33:54 +0200

    fixed spacing

--- src/Processors/AugmentProperties.php
+++ src/Processors/AugmentProperties.php
@@ -63,9 +63,9 @@ class AugmentProperties
                 continue;
             }
             if ($property->enumCallback !== UNDEFINED) {
-              $property->enum = array_values(call_user_func($property->enumCallback));
-              $property->example = $property->enum[0];
-              unset($property->enumCallback);
+                $property->enum = array_values(call_user_func($property->enumCallback));
+                $property->example = $property->enum[0];
+                unset($property->enumCallback);
             }
             $comment = str_replace("\r\n", "\n", $context->comment);
             if (preg_match('/@var\s+(?<type>[^\s]+)([ \t])?(?<description>.+)?$/im', $comment, $varMatches)) {
